fc133034303f09d296eb0df8bbd799750f1106f2
aiden <kingoxpo@gamil.com>
2021-06-24 16:16:34 +0900

    add cookie-parser

--- app_cookie.js
+++ app_cookie.js
@@ -1,5 +1,10 @@
 var express = require("express")
 var app = express();
+app.get('/count', function(req, res){
+    res.cookie('count', 1);
+    res.send('count : ');
+});
+
 app.listen(3003, function(){
     console.log('Connected 3003 port!!!');
 });
