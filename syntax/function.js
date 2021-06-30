bd1f211fde8e7dd04eed8326da5b71c58057beba
Ubuntu <ubuntu@ip-172-31-9-178.ap-northeast-2.compute.internal>
2021-06-30 07:34:46 +0000

    add form.html

---
+++ syntax/function.js
@@ -0,0 +1,27 @@
+num();
+console.log('d');
+console.log('g');
+console.log('d');
+console.log('g');
+num();
+console.log('d');
+console.log('g');
+console.log('d');
+console.log('g');
+console.log('d');
+num();
+console.log('d');
+console.log('d');
+console.log('g');
+console.log('d');
+console.log('d');
+num();
+console.log('d');
+num();
+
+function num(){
+    console.log(8);	
+    console.log(5);	
+    console.log(1);	
+    console.log(6);	
+}
---
+++ syntax/function2.js
@@ -0,0 +1,11 @@
+console.log(Math.round(1.6)); // 2
+console.log(Math.round(1.4)); // 1
+
+
+function sum(first, second){  // parameter
+    console.log('b');
+    return first + second;
+    console.log('a');
+}
+
+console.log(sum(5,23)); // argument
